997393aaea1549168a5697e049643bd275734cd7
nereazj544 <nzjvirtual@gmail.com>
2024-10-01 16:21:45 +0200

    actividades del PDF UD2

--- src/main/java/fp/dam/psp/EvPrimera/TEMA2/ActividadesPDF/Actividad6/Ejemplo2_PrioridadHilo.java
+++ src/main/java/fp/dam/psp/EvPrimera/TEMA2/ActividadesPDF/Actividad6/Ejemplo2_PrioridadHilo.java
@@ -21,21 +21,26 @@ public class Ejemplo2_PrioridadHilo implements Runnable {
         Scanner sc = new Scanner(System.in);
 
         Runnable tarea = new Ejemplo2_PrioridadHilo();
-        System.out.println("> Prioridad del Hilo (1-10)");
-        int p = sc.nextInt();
 
-        if (p <= Thread.MIN_PRIORITY) {
-            System.out.println("> La prioridad minima es 1");
-            p = Thread.MIN_PRIORITY;
-        } else if (p >= Thread.MAX_PRIORITY) {
-            System.out.println("> La prioridad maxima es 10");
-            p = Thread.MAX_PRIORITY;
-        } else if (p == Thread.NORM_PRIORITY) {
-            System.out.println("> La prioridad por defecto es 5");
-            p = Thread.NORM_PRIORITY;
-        }
-
+        System.out.println(
+                "> Si quieres que salga la minima proiridad pon los numeros en negativo o simplemente poniendo 1");
+        System.out.println(
+                "> Si quieres que salga la maxima proiridad pon los numeros en mayores a 10 o simplemente poniendo 10");
+        System.out.println("> Si quieres que salga la proiridad de defecto pon el numero 5");
         for (int i = 1; i <= 3; i++) {
+            System.out.println("> Prioridad del Hilo (1-10):");
+            int p = sc.nextInt();
+
+            if (p <= Thread.MIN_PRIORITY) {
+                System.out.println("> La prioridad minima es 1");
+                p = Thread.MIN_PRIORITY;
+            } else if (p >= Thread.MAX_PRIORITY) {
+                System.out.println("> La prioridad maxima es 10");
+                p = Thread.MAX_PRIORITY;
+            } else if (p == Thread.NORM_PRIORITY) {
+                System.out.println("> La prioridad por defecto es 5");
+                p = Thread.NORM_PRIORITY;
+            }
             // if (p < Thread.MIN_PRIORITY || p > Thread.MAX_PRIORITY) {
             // if (p <= 1 || p <= Thread.MIN_PRIORITY) {
             // System.out.println("> La prioridad minima es 1");
