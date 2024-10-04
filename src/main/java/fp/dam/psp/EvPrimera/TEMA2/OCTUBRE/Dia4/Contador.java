877e20e2447914ad13694590b6f75c12beafbdf6
Tarde <nzjvirtual@gmail.com>
2024-10-04 19:20:50 +0200

    Clase día 4 de octubre

---
+++ src/main/java/fp/dam/psp/EvPrimera/TEMA2/OCTUBRE/Dia4/Contador.java
@@ -0,0 +1,13 @@
+package fp.dam.psp.EvPrimera.TEMA2.OCTUBRE.Dia4;
+public class Contador {
+    private int n;
+    public Contador(int n) {
+        this.n = n;
+    }
+    public void inc() {
+        n++;
+    }
+    public int get() {
+        return n;
+    }
+}
