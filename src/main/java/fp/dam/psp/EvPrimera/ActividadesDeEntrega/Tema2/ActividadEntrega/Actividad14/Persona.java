fa7df925f03cfd0ecc44e62fd0e7a8ebd69ad6e3
nereazj544 <nzjvirtual@gmail.com>
2024-10-19 00:21:36 +0200

    Actividades de Entrega

--- src/main/java/fp/dam/psp/EvPrimera/ActividadesDeEntrega/Tema2/ActividadEntrega/Actividad14/Persona.java
+++ src/main/java/fp/dam/psp/EvPrimera/ActividadesDeEntrega/Tema2/ActividadEntrega/Actividad14/Persona.java
@@ -1,23 +1,39 @@
 package fp.dam.psp.EvPrimera.ActividadesDeEntrega.Tema2.ActividadEntrega.Actividad14;
 
+import java.util.Random;
+
 public class Persona extends Thread {
-    public Persona(int id) {
-        super("Persona " + id);
+    private static int c = 0;
+    private int id;
+    private Banco b;
+    private Random r = new Random();
+
+    public Persona(Banco b) {
+        this.id = ++c;
+        this.b = b;
     }
 
     @Override
     public void run() {
         try {
-            System.out.println(getName() + " pasea por el parque");
-            Thread.sleep((long) (Math.random() * 2000 + 1000)); // tiempo paseando
-            System.out.println(getName() + " llega al banco");
-            if (Actividad14.BANCO.availablePermits() == 0)
-                System.out.println("banco lleno, " + getName() + " espera");
-            Actividad14.BANCO.acquire();
-            System.out.println(getName() + " se ha sentado");
-            Thread.sleep((long) (Math.random() * 500)); // tiempo sentada
-            Actividad14.BANCO.release();
-            System.out.println(getName() + " se ha levantado");
-        } catch (InterruptedException e) {}
+
+            while (true) {
+                int tPaseo = r.nextInt(MAX_PRIORITY) + 1000;
+                System.out.println("Persona " + id + " paseando");
+                Thread.sleep(tPaseo);
+                System.out.println("Persona " + id + " intenta sentarse");
+                b.sentarse();
+                System.out.println("Persona " + id + " se sienta");
+                
+                int tD = r.nextInt(MIN_PRIORITY) + 100;
+                b.levantarse();
+                System.out.println("Persona " + id + " se levanta");
+                
+
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
+
 }
