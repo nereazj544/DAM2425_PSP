1b5745484aa8522e476f09f561fc7c4e2846feea
Tarde <nzjvirtual@gmail.com>
2024-10-18 19:39:11 +0200

    TODO

--- src/main/java/fp/dam/psp/EvPrimera/ActividadesDeEntrega/Tema2/ActividadEntrega/Actividad14/Actividad14.java
+++ src/main/java/fp/dam/psp/EvPrimera/ActividadesDeEntrega/Tema2/ActividadEntrega/Actividad14/Actividad14.java
@@ -1,4 +1,17 @@
 package fp.dam.psp.EvPrimera.ActividadesDeEntrega.Tema2.ActividadEntrega.Actividad14;
 
+import java.util.concurrent.Semaphore;
+
 public class Actividad14 {
+    private static int NUMPERSONAS = 20;
+    private static int NUMPLAZAS = 5;
+    public final static Semaphore BANCO = new Semaphore(NUMPLAZAS);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] personas = new Thread[NUMPERSONAS];
+        for (int i=1; i<NUMPERSONAS; i++)
+            (personas[i] = new Persona(i + 1)).start();
+        for (int i=1; i<NUMPERSONAS; i++)
+            personas[i].join();
+    }
 }
