ab034469faddce7996f0f8d78322dc89593d33a7
Tarde <nzjvirtual@gmail.com>
2024-11-04 19:49:07 +0100

    Dia 04 de noviembre de 2024

--- src/main/java/fp/dam/psp/EvPrimera/TEMA2/Noviembre/Dia4/HiloExecutor/Hilo.java
+++ src/main/java/fp/dam/psp/EvPrimera/TEMA2/Noviembre/Dia4/HiloExecutor/Hilo.java
@@ -1,12 +1,23 @@
 package fp.dam.psp.EvPrimera.TEMA2.Noviembre.Dia4.HiloExecutor;
 
+import fp.dam.psp.EvPrimera.ActividadesDeEntrega.Tema1.ActividadesEntrega.Actividad3.A.Ejecutar;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Hilo implements  Runnable{
+    long tiempo; //tiempo de mimir
+    public Hilo(long tiempo){
+        this.tiempo = tiempo;
+    }
+
+
     public static void main(String[] args) {
         ExecutorService service =
                 Executors.newFixedThreadPool(10); //Hilos maximos a crear
+        for (int i = 0; i < 20; i++) {
+            service.submit(new Hilo(1 * 500));
+        }
 
 
 
@@ -15,6 +26,12 @@ public class Hilo implements  Runnable{
     @Override
     public void run() {
         //DEFINICION DE LA TAREA
-
+        System.out.println(Thread.currentThread().getName() + " dormri " + tiempo);
+        try {
+            Thread.sleep(tiempo);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + " fin ");
     }
 }
