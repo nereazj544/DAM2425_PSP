e682ccf997f936b7936aef1b96030e35c9e9b501
Tarde <nzjvirtual@gmail.com>
2024-11-11 19:26:57 +0100

    Dia 11 de noviembre de 2024

--- src/main/java/fp/dam/psp/EvPrimera/TEMA2/Noviembre/Dia11/Barberia.java
+++ src/main/java/fp/dam/psp/EvPrimera/TEMA2/Noviembre/Dia11/Barberia.java
@@ -1,8 +1,17 @@
 package fp.dam.psp.EvPrimera.TEMA2.Noviembre.Dia11;
 
-public class Barberia {
-    int sillas;
+import java.util.concurrent.Semaphore;
 
+public class Barberia {
+    Semaphore salaEspera;
+    Cliente sillon; //* Almacenaje de la ref. del cliente que se siente
+
+
+
+    public Barberia(int sillas){
+        salaEspera = new Semaphore(sillas);
+
+    }
 
     public boolean enSalaEspera(){
         //? Este metodo retornara verdadero o flaso. Si la sala esta llena retorna falso, si esta vacia (alguna silla) verdaro
