0eb5e7f125d38b802b9c819ded3672c82b4d2046
Tarde <nzjvirtual@gmail.com>
2024-11-11 19:17:14 +0100

    Dia 11 de noviembre de 2024

--- src/main/java/fp/dam/psp/EvPrimera/TEMA2/Noviembre/Dia11/Barberia.java
+++ src/main/java/fp/dam/psp/EvPrimera/TEMA2/Noviembre/Dia11/Barberia.java
@@ -3,4 +3,11 @@ package fp.dam.psp.EvPrimera.TEMA2.Noviembre.Dia11;
 public class Barberia {
     int sillas;
 
+
+    public boolean enSalaEspera(){
+        //? Este metodo retornara verdadero o flaso. Si la sala esta llena retorna falso, si esta vacia (alguna silla) verdaro
+        return false;
+    }
+
+    public void cortarPelo(){}
 }
