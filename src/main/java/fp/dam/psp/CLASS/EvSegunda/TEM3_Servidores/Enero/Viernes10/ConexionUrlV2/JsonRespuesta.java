f5d0338e225aaa92082532871e723add798dc0e3
Tarde <nzjvirtual@gmail.com>
2025-01-10 21:01:49 +0100

    Dia 10 de Enero

--- src/main/java/fp/dam/psp/CLASS/EvSegunda/TEM3_Servidores/Enero/Viernes10/ConexionUrlV2/JsonRespuesta.java
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/TEM3_Servidores/Enero/Viernes10/ConexionUrlV2/JsonRespuesta.java
@@ -1,4 +1,11 @@
 package fp.dam.psp.CLASS.EvSegunda.TEM3_Servidores.Enero.Viernes10.ConexionUrlV2;
 
+
+/*
+https://github.com/google/gson || https://mvnrepository.com
+ */
 public class JsonRespuesta {
+    String [] facts;
+
+
 }
