6a5e57a199047f6bffb22486787018a9efce9c10
nereazj544 <nzjvirtual@gmail.com>
2025-01-22 16:20:53 +0100

    POKEMON

---
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/ActivdadConTest/Other/PokemonApi/V1/Pokemon.java
@@ -0,0 +1,54 @@
+package fp.dam.psp.CLASS.EvSegunda.ActivdadConTest.Other.PokemonApi.V1;
+
+import java.util.List;
+
+public class Pokemon {
+
+    // ! NOTA SUPER IMPORTANTE HAY QUE PONER LOS NOMBRES TAL CUAL SALEN EN EL JSON,
+    // SINO NO CARRULARRA
+
+    public String name;
+    public List<AbilityWrapper> abilities;
+    public List<StatsWrapper> stats;
+    public List<TypesWrapper> types;
+
+    public static class AbilityWrapper {
+        public Ability ability;
+    }
+
+    public static class TypesWrapper {
+        public Types type;
+    }
+
+    public static class StatsWrapper {
+        public Stats stat;
+        public int base_stat;
+    }
+
+    public static class Types {
+        public String name;
+    }
+
+    public static class Stats {
+        public String name;
+    }
+
+    public static class Ability {
+        public String name;
+    }
+
+    /*
+     * 
+     * String nombre;
+     * List<Habilidades> habilidades;
+     * 
+     * public static class Habilidades {
+     * public Habilidades habilidades;
+     * }
+     * 
+     * public static class Habi {
+     * public String nombre;
+     * 
+     * }
+     */
+}
