16ff54d28a7b1dd67a6416b729b468b13cc9253c
nereazj544 <nzjvirtual@gmail.com>
2025-01-23 16:38:26 +0100

    . . .

---
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Jueves23/Ejemplos/Ejemplo1.java
@@ -0,0 +1,14 @@
+package fp.dam.psp.CLASS.EvSegunda.Tema5_ProgSegura.Jueves23.Ejemplos;
+
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        String[] t = { "java.class.path", "java.home", "java.vendor", "java.version",
+                "os.name", "os.version", "user.dir", "user.home", "user.name" };
+
+        System.out.println("PROPIEDAD               VALOR");
+        for (int i = 0; i < t.length; i++) {
+            System.out.format("%-20s ==> %s\n", t[i], System.getProperty(t[i]));
+        }
+
+    }
+}
