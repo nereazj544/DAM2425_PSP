4e856fd69d55af6b18cc463b26c157646fd55499
nereazj544 <nzjvirtual@gmail.com>
2025-01-23 17:15:10 +0100

    . . .

---
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Jueves23/ActidadPDF/Activdad1/Servidor.java
@@ -0,0 +1,24 @@
+package fp.dam.psp.CLASS.EvSegunda.Tema5_ProgSegura.Jueves23.ActidadPDF.Activdad1;
+
+//! Import
+import java.net.*;
+import java.util.concurrent.*;
+
+public class Servidor {
+    public static void main(String[] args) {
+        try (ServerSocket sScok = new ServerSocket(6000)) {
+
+            ExecutorService exSer = Executors.newFixedThreadPool(100);
+
+            System.out.println(". . . Servidor iniciando . . . ");
+            while (true) {
+                Socket sck = sScok.accept();
+                sck.setSoTimeout(60000);
+                exSer.execute(new RequestTask(sck));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
