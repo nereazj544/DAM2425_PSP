8aee76bb7883de91b7f9615d8fcc13f0a58e8a4a
nereazj544 <nzjvirtual@gmail.com>
2025-01-24 23:40:31 +0100

    . . .

--- src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Viernes24/CertificacionDeClaves/Claves.java
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Viernes24/CertificacionDeClaves/Claves.java
@@ -6,15 +6,17 @@ import java.security.PrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 
 public class Claves {
-    PrivateKey key;
-    try(DataInputStream in = new DataInputStream
-            (new DataInputStream("rsa_priv.p8"))){
-        byte [] by = new byte[in.available()];
-        in.read(by);
-        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(by);
-        KeyFactory kf = KeyFactory.getInstance("rsa");
-        key = kf.generatePrivate(spec);
-    }
+    public static void main(String[] args) {
+        PrivateKey pKey;
 
+        try (DataInputStream in = new DataInputStream(new DataInputStream("rsa_priv.p8"))) {
+            byte[] by = new byte[in.available()];
+            in.read(by);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(by);
+            KeyFactory kf = KeyFactory.getInstance("rsa");
+            pKey = kf.generatePrivate(spec);
+        }
+
+    }
 
 }
