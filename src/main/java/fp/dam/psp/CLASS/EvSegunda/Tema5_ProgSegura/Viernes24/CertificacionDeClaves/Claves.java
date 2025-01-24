7da7ab8b5ba079d428acaa6ec0f48f45701f6b2f
Tarde <nzjvirtual@gmail.com>
2025-01-24 21:31:20 +0100

    Dia 24 de Enero

---
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Viernes24/CertificacionDeClaves/Claves.java
@@ -0,0 +1,20 @@
+package fp.dam.psp.CLASS.EvSegunda.Tema5_ProgSegura.Viernes24.CertificacionDeClaves;
+
+import java.io.DataInputStream;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+public class Claves {
+    PrivateKey key;
+    try(DataInputStream in = new DataInputStream
+            (new DataInputStream("rsa_priv.p8"))){
+        byte [] by = new byte[in.available()];
+        in.read(by);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(by);
+        KeyFactory kf = KeyFactory.getInstance("rsa");
+        key = kf.generatePrivate(spec);
+    }
+
+
+}
--- src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Viernes24/Readme.md
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Viernes24/Readme.md
@@ -1,1 +1,5 @@
-# Tema 5: 
+# Tema 5: Técnicas de Programación Segura
+
+Instalacion de Cygwin para poder usar OpenSSL
+
+
