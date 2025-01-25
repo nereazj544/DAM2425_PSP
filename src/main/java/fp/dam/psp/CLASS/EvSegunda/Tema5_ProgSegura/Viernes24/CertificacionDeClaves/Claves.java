af0acfe28a536e25249e4bc430b27d0e96be71ca
nereazj544 <nzjvirtual@gmail.com>
2025-01-25 01:02:45 +0100

    Actualizar ruta de archivo para la carga de clave privada y agregar manejo de excepciones

--- src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Viernes24/CertificacionDeClaves/Claves.java
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Viernes24/CertificacionDeClaves/Claves.java
@@ -1,6 +1,7 @@
 package fp.dam.psp.CLASS.EvSegunda.Tema5_ProgSegura.Viernes24.CertificacionDeClaves;
 
 import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -9,12 +10,14 @@ public class Claves {
     public static void main(String[] args) {
         PrivateKey pKey;
 
-        try (DataInputStream in = new DataInputStream(new DataInputStream("rsa_priv.p8"))) {
+        try (DataInputStream in = new DataInputStream(new FileInputStream("src\\main\\java\\fp\\dam\\psp\\CLASS\\EvSegunda\\Tema5_ProgSegura\\Viernes24\\CertificacionDeClaves\\certs\\rsa_priv.p8"))) {
             byte[] by = new byte[in.available()];
             in.read(by);
             PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(by);
             KeyFactory kf = KeyFactory.getInstance("rsa");
             pKey = kf.generatePrivate(spec);
+        } catch (Exception e) {
+            // TODO: handle exception
         }
 
     }
