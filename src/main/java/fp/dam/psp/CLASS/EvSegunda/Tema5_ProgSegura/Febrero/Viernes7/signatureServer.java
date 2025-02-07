6ce1cedb1879271fda79b55e66dc5961e2539e73
Tarde <nzjvirtual@gmail.com>
2025-02-07 20:50:07 +0100

    Dia 07 de Febrero

--- src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Febrero/Viernes7/signatureServer.java
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Febrero/Viernes7/signatureServer.java
@@ -11,6 +11,30 @@ Firmar = see. bytes
 se hace la firma y la verificacion
  */
 
-public class signatureServer {
 
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class signatureServer {
+    public static void main(String[] args) {
+        try(ServerSocket s = new ServerSocket(6000)){
+
+            ExecutorService ExSer = Executors.newFixedThreadPool(100);
+            System.out.println(" ");
+            System.out.println(". . . Servidor iniciando . . . ");
+            System.out.println(". . . Puerto en escucha: 6000 . . . ");
+
+            while (true){
+                Socket sck = s.accept();
+                sck.setSoTimeout(6000);
+                ExSer.execute(new TaskServerSigna(sck));
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
