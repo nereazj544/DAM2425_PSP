f0a8d89355cac0f89e58bbcab4c90bb7cdd4cf89
nereazj544 <nzjvirtual@gmail.com>
2025-02-10 21:42:17 +0100

    Agregar archivo de firma digital keystore.p12

--- src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Febrero/Tarea/SignaureServerAndClient/signandverifyclient/MainPanel.java
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Febrero/Tarea/SignaureServerAndClient/signandverifyclient/MainPanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.security.*;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
@@ -16,7 +17,8 @@ public class MainPanel extends JPanel {
 
     private final KeyStore ks;
     private static final ArrayList<String> aliases = new ArrayList<>();
-    private static final String [] hashAlgorithms = {"SHA224", "SHA256", "SHA384", "SHA512", "SHA3-224", "SHA3-256", "SHA3-384", "SHA3-512"};
+    private static final String[] hashAlgorithms = { "SHA224", "SHA256", "SHA384", "SHA512", "SHA3-224", "SHA3-256",
+            "SHA3-384", "SHA3-512" };
     private final JComboBox<String> algorithmComboBox;
     private final JComboBox<String> aliasComboBox;
     private final JTextArea info;
@@ -25,8 +27,12 @@ public class MainPanel extends JPanel {
         super(new GridBagLayout());
         ks = KeyStore.getInstance("PKCS12");
         ks.load(ClassLoader.getSystemResourceAsStream("keystore.p12"), "practicas".toCharArray());
-
+        // TODO
         // Almacenar los alias en el ArrayList aliases
+
+        Enumeration<String> aliasEnum = ks.aliases();
+        while (aliasEnum.hasMoreElements())
+            aliases.add(aliasEnum.nextElement());
 
         // ********************************************************************************************************************
 
@@ -40,7 +46,8 @@ public class MainPanel extends JPanel {
         constraints.gridy = 1;
         algorithmComboBox = addComboBox("Algoritmo", hashAlgorithms, 0, constraints, null);
         addSeparator(1, constraints);
-        aliasComboBox = addComboBox("Certificado", aliases.toArray(new String[0]), 2, constraints, this::certificateListener);
+        aliasComboBox = addComboBox("Certificado", aliases.toArray(new String[0]), 2, constraints,
+                this::certificateListener);
         info = new JTextArea(30, 80);
         info.setEditable(false);
         constraints.gridx = 0;
@@ -57,8 +64,7 @@ public class MainPanel extends JPanel {
         button.setToolTipText(toolTip);
         button.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createEmptyBorder(10, 10, 10, 10),
-                button.getBorder()
-        ));
+                button.getBorder()));
         c.gridx = x;
         c.weightx = 1;
         add(button, c);
@@ -68,14 +74,14 @@ public class MainPanel extends JPanel {
         JSeparator separator = new JSeparator(JSeparator.VERTICAL);
         separator.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createEmptyBorder(0, 10, 0, 10),
-                separator.getBorder()
-        ));
+                separator.getBorder()));
         c.gridx = x;
         c.weightx = 0;
         add(separator, c);
     }
 
-    private JComboBox<String> addComboBox(String title, String [] options, int x, GridBagConstraints c, ActionListener listener) {
+    private JComboBox<String> addComboBox(String title, String[] options, int x, GridBagConstraints c,
+            ActionListener listener) {
         c.gridx = x;
         c.weightx = 1;
         GridBagConstraints constraints = new GridBagConstraints();
@@ -88,8 +94,7 @@ public class MainPanel extends JPanel {
                 BorderFactory.createEmptyBorder(10, 10, 10, 10),
                 BorderFactory.createCompoundBorder(
                         BorderFactory.createLineBorder(Color.DARK_GRAY),
-                        BorderFactory.createEmptyBorder(10, 10, 10, 10)
-        )));
+                        BorderFactory.createEmptyBorder(10, 10, 10, 10))));
         panel.add(new JLabel(title), constraints);
         constraints.gridx = 1;
         constraints.weightx = 1;
@@ -110,10 +115,12 @@ public class MainPanel extends JPanel {
                 info.setText(certificate.toString());
                 info.setCaretPosition(0);
             } catch (KeyStoreException ex) {
-                JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Error al obtener el certificado", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Error al obtener el certificado",
+                        JOptionPane.ERROR_MESSAGE);
             }
         else
-            JOptionPane.showMessageDialog(this, "No se ha seleccionado ningún certificado", "Error al obtener el certificado", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this, "No se ha seleccionado ningún certificado",
+                    "Error al obtener el certificado", JOptionPane.ERROR_MESSAGE);
     }
 
     private void signListener(ActionEvent e) {
@@ -122,13 +129,54 @@ public class MainPanel extends JPanel {
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         fileChooser.setMultiSelectionEnabled(false);
         if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-            // Obtener el alias y el algoritmo seleccionados en los JComboBox aliasComboBox y algorithmComboBox
+            // TODO
+            try {
 
+                String AliasSl = (String) aliasComboBox.getSelectedItem();
+                String AlgoSl = (String) algorithmComboBox.getSelectedItem() + "withRSA";
+
+                PrivateKey pk = (PrivateKey) ks.getKey(AliasSl, "practicas".toCharArray());
+                X509Certificate cer = (X509Certificate) ks.getCertificate(AliasSl);
+
+                Signature sgn = Signature.getInstance(AlgoSl);
+                sgn.initSign(pk);
+
+                File f = fileChooser.getSelectedFile();
+                try (FileInputStream fis = new FileInputStream(f)) {
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = fis.read(buffer)) != -1) {
+                        sgn.update(buffer, 0, len);
+                    }
+
+                }
+
+                byte[] fr = sgn.sign();
+
+                String sgB64 = Base64.getEncoder().encodeToString(fr);
+                String cerB64 = Base64.getEncoder().encodeToString(cer.getEncoded());
+
+                String sgFile = f.getAbsolutePath() + ".signature";
+
+                try (FileWriter fw = new FileWriter(sgFile)) {
+                    fw.write(sgB64 + "#" + cerB64);
+                }
+
+                JOptionPane.showMessageDialog(this, "Firma creada correctamente", "Exito",
+                        JOptionPane.INFORMATION_MESSAGE);
+
+            } catch (Exception ex) {
+                ex.getMessage();
+            }
+            // Obtener el alias y el algoritmo seleccionados en los JComboBox aliasComboBox
+            // y algorithmComboBox
 
             // ********************************************************************************************************************
 
-            //  Firmar el fichero seleccionado con el algoritmo seleccionado y el certificado correspondiente al alias seleccionado
-            //  y guardar la firma en un fichero con el mismo nombre que el fichero original añadiendo la extensión .signature
+            // Firmar el fichero seleccionado con el algoritmo seleccionado y el certificado
+            // correspondiente al alias seleccionado
+            // y guardar la firma en un fichero con el mismo nombre que el fichero original
+            // añadiendo la extensión .signature
 
             // ********************************************************************************************************************
         }
@@ -140,9 +188,45 @@ public class MainPanel extends JPanel {
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         fileChooser.setMultiSelectionEnabled(false);
         if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
-            File signatureFile = new File(file.getAbsolutePath() + ".signature");
-            // Enviar la firma y el fichero al servidor y mostrar el resultado de la verificación en un JOptionPane
+            try {
+                File file = fileChooser.getSelectedFile();
+                File signatureFile = new File(file.getAbsolutePath() + ".signature");
+
+                // TODO
+                if (!signatureFile.exists()) {
+                    JOptionPane.showMessageDialog(this, "No se ha encontrado la firma", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+
+                }
+
+                String sgCont = new String(Files.readAllBytes(signatureFile.toPath()));
+
+                try (Socket sck = new Socket("localhost", 6000);
+                        DataInputStream in = new DataInputStream(sck.getInputStream());
+                        DataOutputStream out = new DataOutputStream(sck.getOutputStream())) {
+
+                    out.writeUTF(sgCont);
+                    out.writeInt((int) file.length());
+                    try (FileInputStream fis = new FileInputStream(file)) {
+                        byte[] buffer = new byte[1024];
+                        int len;
+                        while ((len = fis.read(buffer)) != -1) {
+                            out.write(buffer, 0, len);
+                        }
+                    }
+
+                    String res = in.readUTF();
+                    JOptionPane.showMessageDialog(this, res, "Resultado de la verificación",
+                            JOptionPane.INFORMATION_MESSAGE);
+
+                }
+
+            } catch (Exception e) {
+                e.getMessage();
+            }
+            // Enviar la firma y el fichero al servidor y mostrar el resultado de la
+            // verificación en un JOptionPane
 
             // ********************************************************************************************************************
         }
