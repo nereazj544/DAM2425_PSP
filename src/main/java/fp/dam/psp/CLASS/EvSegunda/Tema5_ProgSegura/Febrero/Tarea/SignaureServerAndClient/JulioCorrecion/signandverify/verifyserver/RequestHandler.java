9270f09f29ee821b0dfa0546a01ee8509de1e11a
Tarde <nzjvirtual@gmail.com>
2025-02-17 19:28:57 +0100

    Dia 17 de Febrero

--- src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Febrero/Tarea/SignaureServerAndClient/JulioCorrecion/signandverify/verifyserver/RequestHandler.java
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/Tema5_ProgSegura/Febrero/Tarea/SignaureServerAndClient/JulioCorrecion/signandverify/verifyserver/RequestHandler.java
@@ -1,11 +1,15 @@
 package fp.dam.psp.CLASS.EvSegunda.Tema5_ProgSegura.Febrero.Tarea.SignaureServerAndClient.JulioCorrecion.signandverify.verifyserver;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 import java.security.KeyPair;
 import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.time.LocalTime;
+import java.util.Base64;
 
 public class RequestHandler implements  Runnable{
     private  final Socket sck;
@@ -26,6 +30,19 @@ public class RequestHandler implements  Runnable{
         try (sck;
              DataInputStream in = new DataInputStream(sck.getInputStream());
              DataOutputStream out = new DataOutputStream(sck.getOutputStream())){
+
+            String [] DataSign = in.readUTF().split("#");
+            Base64.Decoder decoder = Base64.getDecoder();
+            byte[] fileSign = decoder.decode(DataSign[0]);
+
+            String algoHash = DataSign[1];
+            byte[] cerEncode = decoder.decode(DataSign[2]);
+
+            CertificateFactory cf  = CertificateFactory.getInstance("X.509");
+            X509Certificate cer = (X509Certificate)  cf.generateCertificate(new ByteArrayInputStream(cerEncode));
+
+
+
 
 //            keyStore.load( "practicas".toCharArray());
 
