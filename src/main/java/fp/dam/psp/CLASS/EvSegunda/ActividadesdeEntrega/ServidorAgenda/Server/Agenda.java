4086a14cb37c93b2db14c42462bda8983a9213df
nereazj544 <nzjvirtual@gmail.com>
2025-02-22 15:58:54 +0100

    . . .

--- src/main/java/fp/dam/psp/CLASS/EvSegunda/ActividadesdeEntrega/ServidorAgenda/Server/Agenda.java
+++ src/main/java/fp/dam/psp/CLASS/EvSegunda/ActividadesdeEntrega/ServidorAgenda/Server/Agenda.java
@@ -1,11 +1,20 @@
 package fp.dam.psp.CLASS.EvSegunda.ActividadesdeEntrega.ServidorAgenda.Server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+
+import java.security.*;
+import java.security.cert.*;
+import java.security.spec.*;
+
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Agenda implements Runnable {
     private final Socket sck;
+    private static final Map<String, Set<String>> contactos = new ConcurrentHashMap<>();
+    private static PrivateKey pvKey; // Clave privada - Servidor
+    private static PublicKey pubKey; // Clave publica - Cliente
 
     public Agenda(Socket sck) {
         this.sck = sck;
@@ -13,14 +22,92 @@ public class Agenda implements Runnable {
 
     @Override
     public void run() {
-        try(sck;
-            DataInputStream in = new DataInputStream(sck.getInputStream());
-            DataOutputStream out = new DataOutputStream(sck.getOutputStream())) {
+        try (sck;
+                DataInputStream in = new DataInputStream(sck.getInputStream());
+                DataOutputStream out = new DataOutputStream(sck.getOutputStream())) {
 
+            int keyLen = in.readInt();
+            byte[] keyClient = new byte[keyLen];
+            in.readFully(keyClient);
+
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(keyClient);
+            pubKey = keyFactory.generatePublic(pubKeySpec);
+            out.writeUTF("> Recibida clave publica del cliente");
+
+            String m = in.readUTF();
+            out.writeUTF("> Mensaje recibido: " + m);
+
+            String r = proMen(m);
+            String firma = firmarMen(r);
+            out.writeUTF(r);
+            out.writeUTF(firma);
 
         } catch (Exception e) {
-
+            System.out.println(e.getMessage());
         }
 
+    }
+
+    private String firmarMen(String mensjae) throws Exception {
+        Signature sign = Signature.getInstance("SHA256withRSA");
+        sign.initSign(pvKey);
+        sign.update(mensjae.getBytes());
+        return Base64.getEncoder().encodeToString(sign.sign());
+    }
+
+    private String proMen(String m) {
+        String[] partes = m.split(":", 2);
+        if (partes.length < 2 && !m.equals("contactos")) {
+            return "ERROR3\n" + m + " no es un comando valido";
+        }
+
+        String cmd = partes[0].toLowerCase();
+        String arg = "";
+        if (partes.length > 1) {
+            arg = partes[1].trim();
+        }
+
+        switch (cmd) {
+            case "nombre":
+                return añadir(arg);
+
+            case "buscar":
+                return buscar(arg);
+
+            case "eliminar":
+                return elimar(arg);
+
+            case "contactos":
+                return listar(arg);
+
+            default:
+                return "ERROR2\n " + m + " no es un comando valido";
+        }
+
+    }
+
+    private String añadir(String arg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'añadir'");
+    }
+
+    private String buscar(String arg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+    }
+
+    private String elimar(String arg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'elimar'");
+    }
+
+    private String listar(String arg) {
+        if (contactos.isEmpty()) {
+            return "ERROR 2\n No hay contactos";
+        }
+        List<String> lista = new ArrayList<>();
+        Collections.sort(lista);
+        return "OK" + String.join("\n", lista);
     }
 }
